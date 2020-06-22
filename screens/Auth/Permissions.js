afea939407fe5004809b62da601f1e94d0a5057d
geraldaddey <geraldaddey@yahoo.com>
2020-06-22 22:29:11 +0000

    new feature added'

--- screens/Auth/Permissions.js
+++ screens/Auth/Permissions.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import {StyleSheet,View,Text,SafeAreaView} from 'react-native'
-import Button from '../../components/Button'
-import Colors from '../../constants/Colors'
-import Layout from '../../constants/Layout'
-import Fonts from '../../constants/Fonts'
+import React from "react";
+import { StyleSheet, View, Text, SafeAreaView } from "react-native";
+import Button from "../../components/Button";
+import Colors from "../../constants/Colors";
+import Layout from "../../constants/Layout";
+import Fonts from "../../constants/Fonts";
 
 function Permission() {
-    return (
-        <SafeAreaView style={{flex:1}}>
-            <View style={styles.container}>
-                <Text> Permission</Text>
-            </View>
-        </SafeAreaView>
-        );
-} 
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text> Permission</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
 
 export default Permission;
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    color: "green",
+    fontWeight: "bold",
+  },
+});
