524b3cb37908171ec85c4248c1695b2d99ef8099
geraldaddey <geraldaddey@yahoo.com>
2020-06-22 22:35:15 +0000

    customized features

--- components/Countries.js
+++ components/Countries.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import { StyleSheet,Modal,View,FlatList,Image} from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import React from "react";
+import { StyleSheet, Modal, View, FlatList, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-export default function ListofCountry(props){
-    return(
-                <FlatList data={props.data}
-                    keyExtractor={item => item.id}
-                    renderItem={({item})=>(
-                        <View>
-                            <TouchableOpacity>
-                                    <View style={{flexDirection:'row'}}>
-                                         <View>
-                                            
-                                         </View>
-                                        <View>
-                                            <Text>{props.item.country}</Text>
-                                        </View>
-                                    </View>
-                            </TouchableOpacity>
-                        </View>
-                    )}
-                />
-    )
+export default function ListofCountry(props) {
+  return (
+    <FlatList
+      data={props.data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View>
+          <TouchableOpacity>
+            <View style={{ flexDirection: "row" }}>
+              <View></View>
+              <View>
+                <Text styles={styles.container}>{props.item.country}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )}
+    />
+  );
 }
 const styles = StyleSheet.create({
-
-})
+  container: {
+    fontWeight: "bold",
+    color: "#C0C0C0",
+  },
+});
