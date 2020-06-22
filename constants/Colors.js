f254dd0e5e6aaabeec2c01d77cd37e1b9f827d89
geraldaddey <geraldaddey@yahoo.com>
2020-06-22 22:46:20 +0000

    minor color changes

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,9 +1,9 @@
-const tintColor = '#2f95dc';
+const tintColor = "#2f95dc";
 const grey = "#A7A7A7";
 const text = "#5F5D5D";
 const background = "#F8F8F8";
-const border = '#e5e5e5';
-const black = '#686874'
+const border = "#e5e5e5";
+const black = "#686874";
 
 export default {
   tintColor,
@@ -14,11 +14,11 @@ export default {
   background,
   tabIconDefault: grey,
   tabIconSelected: "black",
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
+  tabBar: "#4a1b1b",
+  errorBackground: "red",
+  errorText: "#fff",
+  warningBackground: "#ba5c1e",
+  warningText: "#666804",
   noticeBackground: tintColor,
-  noticeText: '#fff',
+  noticeText: "#fff",
 };
